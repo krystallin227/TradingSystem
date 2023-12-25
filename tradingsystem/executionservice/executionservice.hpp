d0b0c0e5d68acf63c99b3ae5573ffaf6ceec8d55
krystallin227 <xinyi.lin.baruchmfe@gmail.com>
2023-12-25 03:25:29 -0500

    add implementation

--- tradingsystem/executionservice/executionservice.hpp
+++ tradingsystem/executionservice/executionservice.hpp
@@ -9,6 +9,10 @@
 #define EXECUTION_SERVICE_HPP
 
 #include <string>
+#include <iostream>
+#include <random>
+#include <set>
+
 #include "..\soa.hpp"
 #include "..\marketdataservice\marketdataservice.hpp"
 
@@ -29,11 +33,16 @@ public:
   // ctor for an order
   ExecutionOrder(const T &_product, PricingSide _side, string _orderId, OrderType _orderType, double _price, double _visibleQuantity, double _hiddenQuantity, string _parentOrderId, bool _isChildOrder);
 
+  //default ctor
+  ExecutionOrder() = default;
   // Get the product
   const T& GetProduct() const;
 
   // Get the order ID
   const string& GetOrderId() const;
+
+  //Get the pricing side
+  PricingSide GetPricingSide() const;
 
   // Get the order type on this order
   OrderType GetOrderType() const;
@@ -93,6 +102,12 @@ const string& ExecutionOrder<T>::GetOrderId() const
 }
 
 template<typename T>
+PricingSide ExecutionOrder<T>::GetPricingSide() const
+{
+	return side;
+}
+
+template<typename T>
 OrderType ExecutionOrder<T>::GetOrderType() const
 {
 	return orderType;
@@ -130,7 +145,7 @@ bool ExecutionOrder<T>::IsChildOrder() const
 
 
 /**
-* An algo execution that process algo execution.
+* AlgoExecution
 * Type T is the product type.
 */
 template<typename T>
@@ -147,21 +162,61 @@ public:
 	ExecutionOrder<T>* GetExecutionOrder() const;
 
 private:
-	ExecutionOrder<T>* executionOrder;
+	ExecutionOrder<T>* execution_order;
 
 };
 
 template<typename T>
 AlgoExecution<T>::AlgoExecution(const T& _product, PricingSide _side, string _orderId, OrderType _orderType, double _price, long _visibleQuantity, long _hiddenQuantity, string _parentOrderId, bool _isChildOrder)
 {
-	executionOrder = new ExecutionOrder<T>(_product, _side, _orderId, _orderType, _price, _visibleQuantity, _hiddenQuantity, _parentOrderId, _isChildOrder);
+	execution_order = new ExecutionOrder<T>(_product, _side, _orderId, _orderType, _price, _visibleQuantity, _hiddenQuantity, _parentOrderId, _isChildOrder);
 }
 
 template<typename T>
 ExecutionOrder<T>* AlgoExecution<T>::GetExecutionOrder() const
 {
-	return executionOrder;
+	return execution_order;
 }
+
+//random order id generator - implemented by chatgpt
+class OrderIDGenerator 
+{
+private:
+	std::set<std::string> generatedIDs;
+	std::mt19937 rng;
+	std::uniform_int_distribution<> charDist;
+	const int idLength;
+
+	std::string generateRandomID() 
+	{
+		std::string id;
+		for (int i = 0; i < idLength; ++i) {
+			char c = static_cast<char>('A' + charDist(rng)); // Generate a random character
+			id += c;
+		}
+		return id;
+	}
+
+public:
+	OrderIDGenerator(int length) : idLength(length) {
+		// Initialize the random number generator with a random device
+		std::random_device rd;
+		rng = std::mt19937(rd());
+
+		// Set the range for the characters, for example, from 'A' to 'Z'
+		charDist = std::uniform_int_distribution<>(0, 25);
+	}
+
+	std::string generateUniqueID() {
+		std::string newID;
+		do {
+			newID = generateRandomID(); // Generate a random string ID
+		} while (generatedIDs.find(newID) != generatedIDs.end()); // Check if it's unique
+
+		generatedIDs.insert(newID); // Insert the unique ID into the set
+		return newID;
+	}
+};
 
 /**
 * Pre-declearations to avoid errors.
@@ -180,11 +235,12 @@ class AlgoExecutionService : public Service<string, AlgoExecution<T>>
 
 private:
 
-	map<string, AlgoExecution<T>> algoExecutions;
+	map<string, AlgoExecution<T>> algo_executions;
 	vector<ServiceListener<AlgoExecution<T>>*> listeners;
-	AlgoExecutionToMarketDataListener<T>* listener;
-	double spread;
-	long count;
+	AlgoExecutionToMarketDataListener<T>* exec_to_mkt_listener;
+	OrderIDGenerator* order_id_gen;
+	double tightest_spread;
+	bool bid_side; //indicator that we are on the bid side
 
 public:
 
@@ -215,11 +271,12 @@ public:
 template<typename T>
 AlgoExecutionService<T>::AlgoExecutionService()
 {
-	algoExecutions = map<string, AlgoExecution<T>>();
+	algo_executions = map<string, AlgoExecution<T>>();
 	listeners = vector<ServiceListener<AlgoExecution<T>>*>();
-	listener = new AlgoExecutionToMarketDataListener<T>(this);
-	spread = 1.0 / 128.0;
-	count = 0;
+	exec_to_mkt_listener = new AlgoExecutionToMarketDataListener<T>(this);
+	order_id_gen = new OrderIDGenerator(8);
+	tightest_spread = 1.0 / 128.0;
+	bid_side = true;
 }
 
 template<typename T>
@@ -228,13 +285,13 @@ AlgoExecutionService<T>::~AlgoExecutionService() {}
 template<typename T>
 AlgoExecution<T>& AlgoExecutionService<T>::GetData(string _key)
 {
-	return algoExecutions[_key];
+	return algo_executions[_key];
 }
 
 template<typename T>
 void AlgoExecutionService<T>::OnMessage(AlgoExecution<T>& _data)
 {
-	algoExecutions[_data.GetExecutionOrder()->GetProduct().GetProductId()] = _data;
+	algo_executions[_data.GetExecutionOrder()->GetProduct().GetProductId()] = _data;
 }
 
 template<typename T>
@@ -252,55 +309,54 @@ const vector<ServiceListener<AlgoExecution<T>>*>& AlgoExecutionService<T>::GetLi
 template<typename T>
 AlgoExecutionToMarketDataListener<T>* AlgoExecutionService<T>::GetListener()
 {
-	return listener;
+	return exec_to_mkt_listener;
 }
 
 template<typename T>
 void AlgoExecutionService<T>::AlgoExecuteOrder(OrderBook<T>& _orderBook)
 {
-	T _product = _orderBook.GetProduct();
-	string _productId = _product.GetProductId();
-	PricingSide _side;
-	string _orderId = GenerateId();
-	double _price;
-	long _quantity;
+	T product = _orderBook.GetProduct();
+	string product_id = product.GetProductId();
+	string order_id = order_id_gen->generateUniqueID();
+	double price;
+	long qty;
+	PricingSide side;
 
-	BidOffer _bidOffer = _orderBook.GetBidOffer();
-	Order _bidOrder = _bidOffer.GetBidOrder();
-	double _bidPrice = _bidOrder.GetPrice();
-	long _bidQuantity = _bidOrder.GetQuantity();
-	Order _offerOrder = _bidOffer.GetOfferOrder();
-	double _offerPrice = _offerOrder.GetPrice();
-	long _offerQuantity = _offerOrder.GetQuantity();
 
-	if (_offerPrice - _bidPrice <= spread)
+	Order bid_order = _orderBook.GetBidStack()[0];
+	double bid = bid_order.GetPrice();
+
+	Order offer_order = _orderBook.GetOfferStack()[0];
+	double offer = offer_order.GetPrice();
+
+	if (offer - bid <= tightest_spread)
 	{
-		switch (count % 2)
-		{
-		case 0:
-			_price = _bidPrice;
-			_quantity = _bidQuantity;
-			_side = BID;
-			break;
-		case 1:
-			_price = _offerPrice;
-			_quantity = _offerQuantity;
-			_side = OFFER;
-			break;
+		if (bid_side)
+		{	//when looking at bid side, we are a seller
+			price = bid;
+			qty = bid_order.GetQuantity();
+			side = BID;
 		}
-		count++;
-		AlgoExecution<T> _algoExecution(_product, _side, _orderId, MARKET, _price, _quantity, 0, "", false);
-		algoExecutions[_productId] = _algoExecution;
+		else
+		{	//else, we are buyer
+			price = offer;
+			qty = offer_order.GetQuantity();
+			side = OFFER;
+		}
+		bid_side =!bid_side;
+
+		AlgoExecution<T> algo_execution(product, side, order_id, MARKET, price, qty, 0, "", false);
+		algo_executions[product_id] = algo_execution;
 
 		for (auto& l : listeners)
 		{
-			l->ProcessAdd(_algoExecution);
+			l->ProcessAdd(algo_execution);
 		}
 	}
 }
 
 /**
-* Algo Execution Service Listener subscribing data from Market Data Service to Algo Execution Service.
+* AlgoExecutionToMarketDataListener listen to updates from MarketDataService.
 * Type T is the product type.
 */
 template<typename T>
@@ -349,6 +405,9 @@ void AlgoExecutionToMarketDataListener<T>::ProcessRemove(OrderBook<T>& _data) {}
 template<typename T>
 void AlgoExecutionToMarketDataListener<T>::ProcessUpdate(OrderBook<T>& _data) {}
 
+//pre declaration
+template<typename T>
+class ExecutionToAlgoExecutionListener;
 
 /**
  * Service for executing orders on an exchange.
@@ -358,13 +417,143 @@ void AlgoExecutionToMarketDataListener<T>::ProcessUpdate(OrderBook<T>& _data) {}
 template<typename T>
 class ExecutionService : public Service<string,ExecutionOrder <T> >
 {
+private:
+
+	map<string, ExecutionOrder<T>> execution_orders;
+	vector<ServiceListener<ExecutionOrder<T>>*> listeners;
+	ExecutionToAlgoExecutionListener<T>* listener;
 
 public:
 
-  // Execute an order on a market
-  void ExecuteOrder(const ExecutionOrder<T>& order, Market market) = 0;
+	// Constructor and destructor
+	ExecutionService();
+	~ExecutionService();
+
+	// Get data on our service given a key
+	ExecutionOrder<T>& GetData(string _key);
+
+	// The callback that a Connector should invoke for any new or updated data
+	void OnMessage(ExecutionOrder<T>& _data);
+
+	// Add a listener to the Service for callbacks on add, remove, and update events for data to the Service
+	void AddListener(ServiceListener<ExecutionOrder<T>>* _listener);
+
+	// Get all listeners on the Service
+	const vector<ServiceListener<ExecutionOrder<T>>*>& GetListeners() const;
+
+	// Get the listener of the service
+	ExecutionToAlgoExecutionListener<T>* GetListener();
+
+	// Execute an order on a market
+	void ExecuteOrder(ExecutionOrder<T>& order, Market market);
 
 };
+
+template<typename T>
+ExecutionService<T>::ExecutionService()
+{
+	execution_orders = map<string, ExecutionOrder<T>>();
+	listeners = vector<ServiceListener<ExecutionOrder<T>>*>();
+	listener = new ExecutionToAlgoExecutionListener<T>(this);
+}
+
+template<typename T>
+ExecutionService<T>::~ExecutionService() {}
+
+template<typename T>
+ExecutionOrder<T>& ExecutionService<T>::GetData(string _key)
+{
+	return execution_orders[_key];
+}
+
+template<typename T>
+void ExecutionService<T>::OnMessage(ExecutionOrder<T>& _data)
+{
+	execution_orders[_data.GetProduct().GetProductId()] = _data;
+}
+
+template<typename T>
+void ExecutionService<T>::AddListener(ServiceListener<ExecutionOrder<T>>* _listener)
+{
+	listeners.push_back(_listener);
+}
+
+template<typename T>
+const vector<ServiceListener<ExecutionOrder<T>>*>& ExecutionService<T>::GetListeners() const
+{
+	return listeners;
+}
+
+template<typename T>
+ExecutionToAlgoExecutionListener<T>* ExecutionService<T>::GetListener()
+{
+	return listener;
+}
+
+template<typename T>
+void ExecutionService<T>::ExecuteOrder(ExecutionOrder<T>& order, Market market)
+{
+	string product_id = order.GetProduct().GetProductId();
+	execution_orders[product_id] = order;
+
+	for (auto& l : listeners)
+	{
+		l->ProcessAdd(order);
+	}
+}
+
+/**
+* ExecutionToAlgoExecutionListener listens to updates from AlgoExecutionService.
+* Type T is the product type.
+*/
+template<typename T>
+class ExecutionToAlgoExecutionListener : public ServiceListener<AlgoExecution<T>>
+{
+
+private:
+
+	ExecutionService<T>* service;
+
+public:
+
+	// Connector and Destructor
+	ExecutionToAlgoExecutionListener(ExecutionService<T>* _service);
+	~ExecutionToAlgoExecutionListener();
+
+	// Listener callback to process an add event to the Service
+	void ProcessAdd(AlgoExecution<T>& _data);
+
+	// Listener callback to process a remove event to the Service
+	void ProcessRemove(AlgoExecution<T>& _data);
+
+	// Listener callback to process an update event to the Service
+	void ProcessUpdate(AlgoExecution<T>& _data);
+
+};
+
+template<typename T>
+ExecutionToAlgoExecutionListener<T>::ExecutionToAlgoExecutionListener(ExecutionService<T>* _service)
+{
+	service = _service;
+}
+
+template<typename T>
+ExecutionToAlgoExecutionListener<T>::~ExecutionToAlgoExecutionListener() {}
+
+template<typename T>
+void ExecutionToAlgoExecutionListener<T>::ProcessAdd(AlgoExecution<T>& _data)
+{
+	ExecutionOrder<T>* _executionOrder = _data.GetExecutionOrder();
+	service->OnMessage(*_executionOrder);
+	service->ExecuteOrder(*_executionOrder, BROKERTEC);
+}
+
+template<typename T>
+void ExecutionToAlgoExecutionListener<T>::ProcessRemove(AlgoExecution<T>& _data) {}
+
+template<typename T>
+void ExecutionToAlgoExecutionListener<T>::ProcessUpdate(AlgoExecution<T>& _data) {}
+
 
 
 
